b70f8f94ddd7495b73a012ed7fb2c6e785647d26
toxa_O <material.xyz.44@gmail.com>
2025-09-02 15:39:58 +0900

    remove keyboards/

--- keyboards/holykeebs/aztec42/keymaps/vial/config.h
+++
@@ -1,9 +0,0 @@
-/* keyboard uid */
-#define VIAL_KEYBOARD_UID {0x81, 0xE6, 0x71, 0x2D, 0xDD, 0xA3, 0x64, 0x06}
-
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-
-/* default layer count */
-#define DYNAMIC_KEYMAP_LAYER_COUNT 10
-
--- keyboards/holykeebs/sweeq/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Idan Kamara (@idank)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#if !defined(MASTER_RIGHT) && !defined(MASTER_LEFT)
-    #define MASTER_RIGHT
-#endif
-
